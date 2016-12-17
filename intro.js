c10869d07827b33a9823e8b419d5b326f6a93745
katelyj <kjohnston@stuy.edu>
2016-12-17 18:28:43 -0500

    add works

--- intro.js
+++ intro.js
@@ -24,8 +24,22 @@ var mouseOut = function(e) {
 };
 
 
-for (i = 0; i < listElements.length; i++) {
-  listElements[i].addEventListener("click", removeItem);
-  listElements[i].addEventListener("mouseover", mouseOver);
-  listElements[i].addEventListener("mouseout", mouseOut);
+var addItem = function() {
+    var n = document.createElement("li")
+    var num = listElements.length;
+    n.innerHTML = "item " + num;
+    n.addEventListener("click", removeItem);
+    n.addEventListener("mouseover", mouseOver);
+    n.addEventListener("mouseout", mouseOut);
+	  list.appendChild(n);
 };
+
+
+for (i = 0; i < listElements.length; i++) {
+    listElements[i].addEventListener("click", removeItem);
+    listElements[i].addEventListener("mouseover", mouseOver);
+    listElements[i].addEventListener("mouseout", mouseOut);
+};
+
+
+button.addEventListener("click", addItem);
